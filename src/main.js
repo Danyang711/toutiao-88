685c6cf81189458543c5f13b09acf88b6fcea86b
Yang <576261674@qq.com>
2019-11-13 17:53:33 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
+import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less'
 
-Vue.use(ElementUI)
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
