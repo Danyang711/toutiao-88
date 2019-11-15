639956c2d4d6a80239a8b457edaaab7aaf7381c0
Yang <576261674@qq.com>
2019-11-15 21:15:49 +0800

    用户退出

--- src/components/layout-header.vue
+++ src/components/layout-header.vue
@@ -18,7 +18,7 @@
             <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item>账户信息</el-dropdown-item>
                 <el-dropdown-item>git地址</el-dropdown-item>
-                <el-dropdown-item>退出</el-dropdown-item>
+                <el-dropdown-item @click.native="onLogout">退出</el-dropdown-item>
             </el-dropdown-menu>
             </el-dropdown>
         </el-col>
@@ -27,7 +27,31 @@
 
 <script>
 export default {
-
+  methods: {
+    onLogout () {
+      this.$confirm('确认退出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 确认执行这里
+        // 删除 token
+        window.localStorage.removeItem('user-token')
+        // 跳转到登录页
+        this.$router.push('/login')
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => {
+        // 取消执行这里
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
+    }
+  }
 }
 </script>
 
